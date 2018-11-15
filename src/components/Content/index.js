f0511c16ffd66e4fda8431893ae33d6212607f5a
Nikita Fedotov <nikster502@gmail.com>
2018-11-15 19:45:03 +0200

    тестовое задание

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -3,112 +3,41 @@ import classNames from "classnames";
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import "./style.css";
 import Dish from '../Dish';
+import products from './products.json';
+
+//http://localhost:3000/products.json
+
 
 class DishGrid extends Component {
     render() {
         const {  } = this.props;
 
-
+        const parsedJSON = JSON.parse(products);
+        console.log(parsedJSON)
+        // for (let i =0;i<parsedJSON.length;i++){
+        //     console.log(parsedJSON[i])
+        //
+        //
+        // }
         const classes = classNames({
-
-
         });
 
         return (
-
                 <section className="content">
 
                     <h1 className="content-city">Moscow Restaurants</h1>
                     <Grid fluid>
                         <Row>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                               <Dish
-                                   srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                   dishName ="Макдоналдс — Газетный"
-                                   dishPrice ="₽₽ • Бургеры"
-                                   dishTime = "25 - 35 Min"
-                               />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
-                            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
-                                <Dish
-                                    srcImg= "../pngDishs/66f1d3abd4940a817c788c541e9fbeb3.png"
-                                    dishName ="Макдоналдс — Газетный"
-                                    dishPrice ="₽₽ • Бургеры"
-                                    dishTime = "25 - 35 Min"
-                                />
-                            </Col>
+                                {
+                                    parsedJSON.map(product =>{
+                                   return(<Dish
+                                        srcImg= {product.urlImg}
+                                        dishName ={product.productName}
+                                        dishPrice ={product.price+" ₽ • Бургеры"}
+                                        dishTime = "25 - 35 Min"
+                                    />);
+                                   })
+                                }
 
                         </Row>
                     </Grid>
--- src/components/Dish/index.js
+++ src/components/Dish/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import "./style.css";
+import {Col} from "react-flexbox-grid";
 
 
 class Dish extends Component {
@@ -13,16 +14,20 @@ class Dish extends Component {
         });
 
         return (
-
+            <Col className="dish-margin" xs={12} sm={6} md={6} lg={4}>
                <div className="content-dish dish">
-                   <a className="dish-link" href="#">
+                   <div className="dish-link" href="">
                    <img className="dish-img" src={srcImg} alt={altImg}/>
                    <h3 className="dish-name">{dishName}</h3>
                    <p className="dish-price">{dishPrice}</p>
                    <p className="dish-time">{dishTime}</p>
-                   </a>
-               </div>
+                   </div>
 
+                   <button className="bnt-bucket">
+                       добавить в корзину
+                   </button>
+               </div>
+            </Col>
         );
     }
 }
